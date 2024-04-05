64e8d11acc39ecafe77d0a4644e47f7108625521
leoseis <eboscrib2@yahoo.com>
2024-04-05 11:41:02 +0100

    added to the base.html

--- lagos_estate/settings.py
+++ lagos_estate/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'lagos_estate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'new_estate',
+        'USER': 'postgres',
+        'PASSWORD': 'sparkle1',
+        'HOST': 'localhost'
     }
 }
 
