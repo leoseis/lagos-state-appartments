ec2c09e8f0211c1643122122cfc44b6cf78a2add
leoseis <eboscrib2@yahoo.com>
2025-06-15 06:37:24 +0800

    modified the detail page and modified the models

--- appartments/models.py
+++ appartments/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 
 # models.py
-from django.db import models
+class Agent(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 class Appartment(models.Model):
     title = models.CharField(max_length=150)
@@ -12,7 +18,9 @@ class Appartment(models.Model):
     num_bathrooms = models.IntegerField()
     square_footage = models.IntegerField()
     address = models.CharField(max_length=100)
-    cover_image = models.ImageField(upload_to='appartments/covers/', blank=True, null=True)  # main image
+    cover_image = models.ImageField(upload_to='appartments/covers/', blank=True, null=True) 
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE, null=True, blank=True)
+ # main image
 
     def __str__(self):
         return self.title
