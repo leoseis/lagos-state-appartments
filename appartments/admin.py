2cc811fe82e20e410814117ca62ca6483d995378
leoseis <eboscrib2@yahoo.com>
2025-06-16 03:49:20 +0800

    agent firld bug fixed was at the admin.py

--- appartments/admin.py
+++ appartments/admin.py
@@ -1,12 +1,17 @@
-# admin.py
 from django.contrib import admin
-from .models import Appartment, ApartmentImage
+from .models import Appartment, ApartmentImage, Agent  # Added Agent import
 
 class ApartmentImageInline(admin.TabularInline):
     model = ApartmentImage
     extra = 1
 
+@admin.register(Agent)  # Register Agent model
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email']
+
 @admin.register(Appartment)
 class AppartmentAdmin(admin.ModelAdmin):
     inlines = [ApartmentImageInline]
-    list_display = ['title', 'price', 'address']
+    list_display = ['title', 'price', 'address', 'agent']  # Show agent in list view
+    fields = ['title', 'price', 'num_bedrooms', 'num_bathrooms', 
+             'square_footage', 'address', 'cover_image', 'agent']  # Explicit fields
--- appartments/views.py
+++ appartments/views.py
@@ -1,29 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Appartment
 from .forms import AppartmentForm
 
-
-#CRUD CREATE, RETRIEVE,UPDATE,DELETE, List 
-
+# CRUD: Create, Retrieve, Update, Delete, List 
 
 def Appartment_list(request):
-    appartments = Appartment.objects.all()
+    appartments = Appartment.objects.select_related('agent').all()
     context = {
        'appartments': appartments
     }
-    return render(request,'appartments.html', context)
+    return render(request, 'appartments.html', context)
 
 def Appartment_retrieve(request, pk):
-    appartment = Appartment.objects.get(id=pk)
+    appartment = Appartment.objects.select_related('agent').get(id=pk)
     context = {
        'appartment': appartment
     }
-    return render(request,'appartment.html', context)
-
+    return render(request, 'appartment.html', context)
 
 def Appartment_create(request):
     form = AppartmentForm() 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = AppartmentForm(request.POST)
         if form.is_valid():
             form.save()
@@ -32,22 +29,21 @@ def Appartment_create(request):
     context = {
         'form': form
     }
-    return render(request, 'appartment_create.html', context )
+    return render(request, 'appartment_create.html', context)
 
 def Appartment_update(request, pk):
     appartment = Appartment.objects.get(id=pk)
-    form = AppartmentForm(instance=Appartment)
+    form = AppartmentForm(instance=appartment)
 
     if request.method == 'POST':
-     form = AppartmentForm(request.POST, instance=Appartment)
-    if form.is_valid():
+        form = AppartmentForm(request.POST, instance=appartment)
+        if form.is_valid():
             form.save() 
             return redirect('/')
     context = {
          'form': form
     }
-    return render(request, 'appartment_update.html', context )
-
+    return render(request, 'appartment_update.html', context)
 
 def Appartment_delete(request, pk):
     appartment = Appartment.objects.get(id=pk)
